5cb20f1f53d8607dcbf31b8314fed4bc8eb05ee6
Sarangm3 <85629008+Sarangm3@users.noreply.github.com>
2022-01-29 18:53:48 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import react from "react";
 import './style.css'
 
-export default function Navbar()
+export default function Navbar(props)
 {
     return(
         <header>
-            <nav>
+            <nav className={props.darkMode ? "dark": ""}>
                 <img src={require("./images/photo.jpg")} width="20px"/>
                 <h2>my travel.com</h2>
+            <div 
+                className="toggler" 
+                >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
             </nav>
         </header>   
     )
