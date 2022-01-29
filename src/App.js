1bb5d739050b17bd52f144ae16f5119f6cf1abd6
Sarangm3 <85629008+Sarangm3@users.noreply.github.com>
2022-01-29 18:52:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import NavBar from './components/Navbar';
 import Card from './components/Card';
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(true)
+  
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
   const cards = data.map(item=>{
     return(<Card 
           key={item.id}
           item={item}
+          darkMode={darkMode} 
     />)
   }) 
+
   return(
       <div>
-        <NavBar />
-        {cards}
+        <NavBar 
+          toggleDarkMode={toggleDarkMode}
+          darkMode={darkMode} 
+        />
+        <main className={darkMode ? "dark" : ""}>
+          {cards}
+        </main>
       </div>
         
     )
