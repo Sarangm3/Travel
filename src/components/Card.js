f837703e3662cbc9912779d3da677dc548c7aead
Sarangm3 <85629008+Sarangm3@users.noreply.github.com>
2022-01-29 18:54:26 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,16 @@ import react from "react";
 
 export default function Cand(props){
     return(
-        <div className="item">
-            <img src={props.item.imageUrl}/>
-            <div className="description">
-                <span><p>{props.item.location}</p>
-                <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a></span>
-                <h1>{props.item.title}</h1>
-                <p className="date">{props.item.startDate} - {props.item.endDate}</p>
-                <p>{props.item.description}</p>
+            <div className="item">
+                <img src={props.item.imageUrl}/>
+                <div className="description">
+                    <span><p>{props.item.location}</p>
+                    <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a></span>
+                    <h1>{props.item.title}</h1>
+                    <p className="date">{props.item.startDate} - {props.item.endDate}</p>
+                    <p>{props.item.description}</p>
+                </div>
             </div>
-        </div>
+        
     )
 }
